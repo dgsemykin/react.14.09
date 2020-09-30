4d29784acb5eba25969cdbd22bb7e17e11f88482
TatianaKon <kontan2025@gmail.com>
2020-09-30 22:14:01 +0300

    dz5

--- KononovaT/src/components/App/App.jsx
+++ KononovaT/src/components/App/App.jsx
@@ -2,19 +2,46 @@ import React from 'react';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import { CssBaseline } from '@material-ui/core';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import RootRouter from '../../pages/RootRouter/RootRouter';
+import store from '../../store';
 
-const theme = createMuiTheme();
+const theme = createMuiTheme({
+    typography: {
+        fontFamily: 'Raleway, Arial',
+        subtitle1: {
+        fontSize: 12,
+        },
+    },
+    secondary: {
+        light: '#0066ff',
+        main: '#0044ff',
+        contrastText: '#ffcc00',
+    },
+
+    palette: {
+        primary: {
+            main: '#4caf50',
+        },
+    },
+    button: {
+        fontStyle: 'italic',
+        
+    },
+
+});
 
 const App = () => {
-    return (
+return (
+    <Provider store={store}>
         <BrowserRouter>
             <ThemeProvider theme={theme}>
                 <CssBaseline />
                 <RootRouter />
             </ThemeProvider>
         </BrowserRouter>
-    );
+    </Provider>
+);
 };
 
 export default App;
--- KononovaT/src/pages/Chats/Chats.jsx
+++ KononovaT/src/pages/Chats/Chats.jsx
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { v4 as uuidv4 } from 'uuid';
 import produce from 'immer';
+import { connect } from 'react-redux';
 import MessageList from '../../components/MessageList';
 import FormMessage from '../../components/FormMessage';
 import Layout from '../../components/Layout/Layout';
+import { addMessage } from '../../reducers/messagesReducer';
 
 class Chats extends Component {
 state = {
     chats: {
         1: { id: 1, title: 'Чат 1', messageList: [1, 2] },
-        2: { id: 2, title: 'Чат 2', messageList: [1, 2] },
+        2: { id: 2, title: 'Чат 2', messageList: [3, 2] },
         3: { id: 3, title: 'Чат 3', messageList: [2, 3] },
     },
     messages: {
@@ -26,21 +29,22 @@ state = {
         3: {
             id: 3,
             author: 'Bot',
-            message: 'Давай поболтаем, в чате три',
+            message: 'Давай поболтаем, я в третьем чате',
         },
     },
 };
 
+
 componentDidUpdate(prevProps, prevState) {
     const lastMessages = this.messages;
 
-    if (lastMessages[lastMessages.length - 1]?.author !== 'Bot') {
-        setTimeout(() => {
-            this.addMessage({ author: 'Bot', message: 'привет, я бот' });
-        }, 500);
+
+if (lastMessages[lastMessages.length - 1]?.author !== 'Bot') {
+    setTimeout(() => {
+        this.addMessage({ author: 'Bot', message: 'привет, я бот' });
+    }, 500);
     }
 }
-
 get messages() {
     const { id } = this.props.match.params;
     const { chats, messages } = this.state;
@@ -50,27 +54,54 @@ get messages() {
     return [];
 }
 
+
+
+
 addMessage = ({ author, message }) => {
-    const { id } = this.props.match.params;
+    const {id} = this.props.match.params;
     const newId = uuidv4();
 
-    this.setState(({ chats, messages }) => ({
-        chats: {
-            ...chats,
-            [id]: { ...chats[id], messageList: [...chats[id].messageList, newId] },
-        },
-        messages: { ...messages, [newId]: { id: newId, author, message } },
+    this.setState(prevState =>
+        produce(prevState, draft => {
+        draft.chats[id].messageList.push(newId);
+        draft.messages[newId] = { id: newId, author, message };
+        }),
+    );
+};
+
+addChat = () => {
+    const newId = uuidv4();
+    this.setState(({ chats }) => ({
+        chats: { ...chats, [newId]: { id: newId, title: `Чат ${newId}`, messageList: [] } },
     }));
 };
+
+
 
 render() {
     return (
         <Layout>
             <MessageList messages={this.messages} />
             <FormMessage addMessage={this.addMessage} />
+
         </Layout>
     );
 }
 }
 
-export default Chats;
+Chats.propTypes = {
+match: PropTypes.shape({
+    params: PropTypes.objectOf(PropTypes.any),
+}).isRequired,
+};
+
+const mapStateToProps = state => ({
+chats: state.chats.byIds,
+messages: state.messages.byIds,
+});
+
+const mapDispatchToProps = {
+addMessage,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Chats);
